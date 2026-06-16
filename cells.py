9584586bb24a136e6446e6b6a09cb18f5feb7805
Himajori <jorgen.hima@gmail.com>
2026-06-16 12:15:53 +0200

    Add files via upload
    func remove Wall & get_unvisited_neighbors

--- cells.py
+++ cells.py
@@ -4,10 +4,10 @@ class Cell:
         self.y = y
         self.visited = False
         self.walls = {
-            "up": True,
-            "right": True,
-            "down": True,
-            "left": True
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True
         }
 
 
@@ -28,16 +28,36 @@ def get_neighbors(cell, grid):
     height= len(grid)
     x, y = cell.x, cell.y
     if y > 0:
-        neighbors["up"] = grid[y - 1][x]
+        neighbors["N"] = grid[y - 1][x]
     if x < width -1:
-        neighbors["left"] = grid[y][x + 1]
+        neighbors["E"] = grid[y][x + 1]
     if y < height - 1:
-        neighbors["down"] = grid[y + 1][x]
+        neighbors["S"] = grid[y + 1][x]
     if x > 0:
-        neighbors["right"] = grid[y][x - 1]
+        neighbors["W"] = grid[y][x - 1]
 
     return neighbors
 
+OPPOSITE = {
+    "N": "S",
+    "E": "W",
+    "S": "N",
+    "W": "E"
+}
+
+def remove_wall(cell, neighbor, direction):
+    cell.walls[direction] = False
+    neighbor.walls[OPPOSITE[direction]] = False
+
+def get_unvisted_neighbors(cell, grid):
+    neighbors = get_neighbors(cell, grid)
+
+    unvisted_neighbors = {}
+    for dir in neighbors:
+        neighbor = neighbors[dir]
+        if not neighbor.visited:
+            unvisted_neighbors[dir] = neighbor
+    return unvisted_neighbors
 
 grid = create_grid(3,3)
 for row in grid:
@@ -47,5 +67,9 @@ for row in grid:
 cell = grid[0][0]
 neighbors = get_neighbors(cell, grid)
 
-for dir, nei in neighbors.items():
+# for dir, nei in neighbors.items():
+#     print(f"{dir}: ({nei.x}, {nei.y})")
+
+unvisited = get_unvisted_neighbors(cell, grid)
+for dir, nei in unvisited.items():
     print(f"{dir}: ({nei.x}, {nei.y})")
